8a20cd3aff2e5a77fa010d431eff7e59ea877555
wardvisual <wardvisual@gmail.com>
2023-08-11 04:01:34 +0800

    fix: import AppRoutingModule instead of RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { routes } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { AuthComponent } from './views/auth/auth.component';
@@ -9,7 +9,7 @@ import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, AuthComponent],
-  imports: [BrowserModule, RouterModule.forChild(routes)],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   exports: [RouterModule],
   bootstrap: [AppComponent],
