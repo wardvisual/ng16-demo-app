dcf5a3f5319491d3d2bfe90f3c3fb7d7d96cabfe
wardvisual <wardvisual@gmail.com>
2023-08-11 03:49:19 +0800

    chore: generate layout component

---
+++ src/app/layouts/auth-layout/auth-layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-auth-layout',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './auth-layout.component.html',
+  styleUrls: ['./auth-layout.component.scss']
+})
+export class AuthLayoutComponent {
+
+}
---
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './main-layout.component.html',
+  styleUrls: ['./main-layout.component.scss']
+})
+export class MainLayoutComponent {
+
+}
