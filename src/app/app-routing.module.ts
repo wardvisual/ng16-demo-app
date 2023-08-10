29bc4aec1792a540ff026e6d605e8208257a89e9
wardvisual <wardvisual@gmail.com>
2023-08-11 03:42:58 +0800

    chore: add auth and home route views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent, AuthComponent } from '@ng16-demoapp/views';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('@ng16-demoapp/views').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'auth',
+    loadComponent: () =>
+      import('@ng16-demoapp/views').then((m) => m.AuthComponent),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
