4baf347044dd0c2744bc31fdffff0396dc98501c
wardvisual <wardvisual@gmail.com>
2023-08-11 14:03:05 +0800

    chore(views): wip -  setting up auth views screen

--- src/app/views/auth/auth.component.ts
+++ src/app/views/auth/auth.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { SignUp } from './types/auth.types';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
+  form: FormGroup;
 
+  ngOnInit() {
+    this.createSignupForm();
+  }
+
+  createSignupForm() {
+    this.form = new FormGroup<SignUp>({
+      firstName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      lastName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      emailAddress: new FormControl('', [
+        Validators.required,
+        Validators.email,
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+      ]),
+    });
+  }
+
+  onSignup(form: FormGroup) {
+    console.log({ form });
+  }
 }
