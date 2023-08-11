12d7275138bec1f559d64308281bc66b0749c939
wardvisual <wardvisual@gmail.com>
2023-08-11 11:57:16 +0800

    chore(component): wip - creating a usable input component

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+    FormsModule,
+    ReactiveFormsModule,],
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+  styleUrls: ['./input.component.scss'],
 })
 export class InputComponent {
+  @Input() label!: string;
+  @Input() type!: string;
+  @Input() name!: string;
+  @Input() value!: string;
+  @Input() required!: string;
 
+  @Input() formControl!: FormControl;
+
+  handleDisplayErrors() {
+    const { dirty, touched, errors } = this.formControl;
+
+    return dirty && touched && errors;
+  }
 }
