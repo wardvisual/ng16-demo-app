c84a5b16f1cd8fccc3d9057682b63bbdd2d95084
wardvisual <wardvisual@gmail.com>
2023-08-11 14:04:10 +0800

    chore(component): wip - setting up input component

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl } from '@angular/forms';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
@@ -6,24 +6,24 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [CommonModule,
-    FormsModule,
-    ReactiveFormsModule,],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss'],
 })
-export class InputComponent {
-  @Input() label!: string;
-  @Input() type!: string;
-  @Input() name!: string;
-  @Input() value!: string;
-  @Input() required!: string;
+export class InputComponent implements OnInit {
+  @Input() label: string;
+  @Input() type: string;
+  @Input() name: string;
+  @Input() value: string;
+  @Input() required: boolean;
 
-  @Input() formControl!: FormControl;
+  @Input() formControl: FormControl;
 
-  handleDisplayErrors() {
+  ngOnInit() {}
+
+  displayErrors() {
     const { dirty, touched, errors } = this.formControl;
-
+    console.log({ dirty, touched, errors });
     return dirty && touched && errors;
   }
 }
