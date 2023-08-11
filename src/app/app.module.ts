726e64b0d630a19ece4e6257267f564ad1c0224b
wardvisual <wardvisual@gmail.com>
2023-08-12 01:55:02 +0800

    chore(component): import HttpClientModule and Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
@@ -7,12 +8,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent, AuthComponent } from '@ng16-demoapp/views';
 import { ButtonComponent, InputComponent } from '@ng16-demoapp/components';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SignInComponent } from './views/auth/components/sign-in/sign-in.component';
+import { SignUpComponent } from './views/auth/components/sign-up/sign-up.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AuthComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AuthComponent,
+    SignInComponent,
+    SignUpComponent,
+    AuthLayoutComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+
+    /* Components */
     InputComponent,
     ReactiveFormsModule,
     ButtonComponent,
