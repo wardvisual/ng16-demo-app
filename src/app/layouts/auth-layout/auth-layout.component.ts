1d562b67158c705ccc50808f3c0f795c4815c873
wardvisual <wardvisual@gmail.com>
2023-08-11 21:23:08 +0800

    chore(layout): WIP - inherit base auth c omponent

--- src/app/layouts/auth-layout/auth-layout.component.ts
+++ src/app/layouts/auth-layout/auth-layout.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-auth-layout',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './auth-layout.component.html',
-  styleUrls: ['./auth-layout.component.scss']
+  styleUrls: ['./auth-layout.component.scss'],
 })
-export class AuthLayoutComponent {
-
-}
+export class AuthLayoutComponent {}
