f77b3b7433f5773ec217d3e060c0860e5ef4c59d
wardvisual <wardvisual@gmail.com>
2023-08-11 14:02:02 +0800

    chore(types): create auth types

---
+++ src/app/views/auth/types/auth.types.ts
@@ -0,0 +1,9 @@
+import { FormControl } from '@angular/forms';
+
+export interface SignUp {
+  firstName: FormControl;
+  lastName: FormControl;
+  username: FormControl;
+  emailAddress: FormControl;
+  password: FormControl;
+}
