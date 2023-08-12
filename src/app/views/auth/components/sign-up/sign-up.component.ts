f5ebf2cb769d55ea186d7843c71bac3a721838bc
wardvisual <wardvisual@gmail.com>
2023-08-12 15:04:14 +0800

    chore(component): remove constructor

--- src/app/views/auth/components/sign-up/sign-up.component.ts
+++ src/app/views/auth/components/sign-up/sign-up.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
 import { AuthComponent } from 'src/app/views/auth/auth.component';
-import { ApiService } from '@ng16-demoapp/core';
-import { AuthService } from '../../auth.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
 })
-export class SignUpComponent extends AuthComponent {
-  constructor(router: Router, authService: AuthService) {
-    super(router, authService);
-  }
-}
+export class SignUpComponent extends AuthComponent {}
