65f29aad813cf44c36bc1fe1f778f13bdc98b5e7
wardvisual <wardvisual@gmail.com>
2023-08-12 19:06:44 +0800

    chore(env): remove unused apiURL property from env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,6 @@ import customEnv from '../../custom-env';
 
 export const environment = {
   production: false,
-  apiUrl: `http://localhost:3000`,
   supabaseUrl: customEnv.SUPABASE_URL,
   supabaseAnonKey: customEnv.SUPABASE_ANON_KEY,
 };
