8851cf2032d04e2bb826c79cee0da7c0fb5bb470
wardvisual <wardvisual@gmail.com>
2023-08-12 15:03:40 +0800

    chore(env): create and use custom env to load secrets

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,8 @@
+import customEnv from '../../custom-env';
+
 export const environment = {
   production: false,
   apiUrl: `http://localhost:3000`,
+  supabaseUrl: customEnv.SUPABASE_URL,
+  supabaseAnonKey: customEnv.SUPABASE_ANON_KEY,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  apiUrl: ``,
+  supabaseUrl: ``,
+  supabaseAnonKey: ``,
+};
