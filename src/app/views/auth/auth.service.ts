b5467949845049fca61d11ce4e2b9e903f45d52f
wardvisual <wardvisual@gmail.com>
2023-08-12 19:02:51 +0800

    chore(service): implement custom basic custom authentication with supabase

--- src/app/views/auth/auth.service.ts
+++ src/app/views/auth/auth.service.ts
@@ -1,13 +1,66 @@
 import { Injectable } from '@angular/core';
-import { ApiService } from '@ng16-demoapp/core';
+
+import { SignIn, SignUp } from './types/auth.types';
+import { SupabaseService } from '@ng16-demoapp/services';
+import { SupabaseResponse } from '@ng16-demoapp/types';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private apiService: ApiService) {}
+  constructor(private supabaseService: SupabaseService) {}
 
-  public getDataFromApi(): any {
-    this.apiService.get('').subscribe();
+  public async register(user: SignUp): Promise<SupabaseResponse> {
+    const userFromDb: any = await this.supabaseService.supabase
+      .from('users')
+      .select()
+      .or(
+        `username.eq.${user?.username},emailAddress.eq.${user?.username},username.eq.${user?.emailAddress},emailAddress.eq.${user?.emailAddress}`
+      ); /* Checks if a registered user already exists with the provided email or username. */
+
+    if (userFromDb.data?.length) {
+      return {
+        iSuccess: false,
+        message: `The ${user.username ? 'username' : 'email'} is already taken`,
+      } satisfies SupabaseResponse;
+    }
+
+    await this.supabaseService.supabase.from('users').insert([user]);
+
+    return {
+      iSuccess: true,
+      message: 'You are now registered!',
+    } satisfies SupabaseResponse;
+  }
+
+  public async login(user: SignIn): Promise<SupabaseResponse> {
+    const userFromDb: any = await this.supabaseService.supabase
+      .from('users')
+      .select()
+      .or(
+        `username.eq.${user?.username},emailAddress.eq.${user?.username},username.eq.${user?.emailAddress},emailAddress.eq.${user?.emailAddress}`
+      ); /* Checks if a registered user already exists with the provided email or username. */
+
+    if (!userFromDb.data?.length) {
+      return {
+        iSuccess: false,
+        message: `Invalid username or password`,
+      } satisfies SupabaseResponse;
+    }
+
+    if (
+      userFromDb.data?.length &&
+      userFromDb.data[0]?.password !== user?.password
+    ) {
+      return {
+        iSuccess: false,
+        message: `Invalid username or password`,
+      } satisfies SupabaseResponse;
+    }
+
+    return {
+      iSuccess: true,
+      message: 'You are now login!',
+    } satisfies SupabaseResponse;
   }
 }
