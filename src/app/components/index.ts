a73d21ed9ab22119be71d3d344bf82f324efcfa6
wardvisual <wardvisual@gmail.com>
2023-08-13 19:48:07 +0800

    chore(component): export toast service and component

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -2,6 +2,8 @@
 export * from './button/button.component';
 export * from './input/input.component';
 export * from './modal/modal.component';
+export * from './toast/toast.component';
 
 // Services
 export * from './modal/modal.service';
+export * from './toast/toast.service';
