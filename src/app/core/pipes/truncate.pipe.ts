87e123a471d40bb10f6e8611f88a35014fcca6e5
wardvisual <wardvisual@gmail.com>
2023-08-13 20:15:43 +0800

    chore(pipes): create truncate string pipe

---
+++ src/app/core/pipes/truncate.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'truncate',
+})
+export class TruncatePipe implements PipeTransform {
+  transform(value: string, length: number): string {
+    if (value.length > length) {
+      return value.substring(0, length) + '...';
+    }
+    return value;
+  }
+}
