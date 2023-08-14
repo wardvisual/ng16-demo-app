c3787e963489853c87b6f0119b081a8352e9d99a
wardvisual <wardvisual@gmail.com>
2023-08-14 16:00:17 +0800

    chore: add global types for auth

---
+++ src/app/core/types/auth.type.ts
@@ -0,0 +1,15 @@
+import { FormControl } from '@angular/forms';
+
+export interface SignUp {
+  firstName: FormControl;
+  lastName: FormControl;
+  username: FormControl;
+  emailAddress: FormControl;
+  password: FormControl;
+}
+
+export interface SignIn {
+  username: FormControl;
+  emailAddress?: FormControl;
+  password: FormControl;
+}
