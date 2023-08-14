3513f3310ea196de3a22db37b299ee709e267fea
wardvisual <wardvisual@gmail.com>
2023-08-14 22:48:05 +0800

    chore(types): move NoteTypes directory

--- src/app/core/types/note.type.ts
+++ src/app/core/types/note.type.ts
@@ -1,6 +1,14 @@
 import { FormControl } from '@angular/forms';
 
 export interface Note {
+  id: string;
+  title: string;
+  content: string;
+  createdAt: string;
+  userId: string;
+}
+
+export interface CreateNote {
   title: FormControl;
   content: FormControl;
   createdAt: FormControl;
