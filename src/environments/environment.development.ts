dd1938f46363f9a2e2365fb6b483b2a14e65c64f
wardvisual <wardvisual@gmail.com>
2023-08-14 22:47:11 +0800

    ref(env): add comment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,8 @@
 import customEnv from '../../custom-env';
 
+/**
+ * @desc Alternatively, I created a `customEnv.js` file at the root of the project and put it in .gitignore. Just to make things easier.
+ * */
 export const environment = {
   production: false,
   supabaseUrl: customEnv.SUPABASE_URL,
