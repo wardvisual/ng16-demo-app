1e6df70e68bf42b75505376618fa87a195bef5ef
wardvisual <wardvisual@gmail.com>
2023-08-14 22:48:41 +0800

    chore(component): implement view notes logic

--- src/app/views/notes/features/notes-listing/notes-listing.component.ts
+++ src/app/views/notes/features/notes-listing/notes-listing.component.ts
@@ -1,10 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, Signal, computed } from '@angular/core';
+import { ModalService } from '@ng16-demoapp/components';
+import { Note } from '@ng16-demoapp/types';
 
 @Component({
   selector: 'app-notes-listing',
   templateUrl: './notes-listing.component.html',
   styleUrls: ['./notes-listing.component.scss'],
 })
-export class NotestListingComponent {
-  @Input() notes;
+export class NotestListingComponent implements OnInit {
+  @Input() notes: Signal<Note[]>;
+  @Input() notesList: number;
+  @Input() callback: (note: Note) => void;
+
+  selectedNote: Note;
+
+  constructor(protected modalService: ModalService) {}
+
+  ngOnInit(): void {}
+
+  toggleViewNote(note: Note) {
+    this.selectedNote = note;
+    this.callback(note);
+
+    this.modalService.toggleModal(note.id, true);
+  }
 }
