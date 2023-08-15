211f40db2205c2dc03e3e473e96021509aef35b8
wardvisual <wardvisual@gmail.com>
2023-08-15 10:23:50 +0800

    chore(service): add comment

--- src/app/core/services/loader.service.ts
+++ src/app/core/services/loader.service.ts
@@ -6,10 +6,22 @@ import { Injectable } from '@angular/core';
 export class LoaderService {
   private loadingStates: Map<any, boolean> = new Map<any, boolean>();
 
+  /**
+   * Sets the loading state for a specific component.
+   *
+   * @param {any} component - The component to set the loading state for.
+   * @param {boolean} loading - The loading state to set.
+   */
   setLoading(component: any, loading: boolean) {
     this.loadingStates.set(component, loading);
   }
 
+  /**
+   * Returns the loading state for a given component.
+   *
+   * @param {any} component - The component to check the loading state for.
+   * @return {boolean} The loading state of the component. Defaults to false if not found.
+   */
   getLoading(component: any): boolean {
     return this.loadingStates.get(component) || false;
   }
