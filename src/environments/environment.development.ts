b2dfa930dcc2c6ce09a7eb08eee02a54e6f81fd5
wardvisual <wardvisual@gmail.com>
2023-08-15 21:35:21 +0800

    chore(env): update env comment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,9 @@ import customEnv from '../../custom-env';
 
 /**
  * @desc Alternatively, I created a `customEnv.js` file at the root of the project and put it in .gitignore. Just to make things easier.
+ * @env_shape
+ *  SUPABASE_URL: string
+ *  SUPABASE_ANON_KEY: string
  * */
 export const environment = {
   production: false,
