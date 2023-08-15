4acb8539c8dad46414d85b81400b7ac2ef1cfc1d
wardvisual <wardvisual@gmail.com>
2023-08-15 20:31:47 +0800

    chore(service): remove routing service

--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -1,7 +1,6 @@
 export * from './supabase.service';
 export * from './local-storage.service';
 export * from './loader.service';
-export * from './routing.service';
 export * from './auth.service';
 export * from './date.service';
 export * from './notes.service';
