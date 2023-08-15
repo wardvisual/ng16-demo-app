61a8fdb0df091573211c4ad9ae4085e9c189ec8a
wardvisual <wardvisual@gmail.com>
2023-08-15 13:48:03 +0800

    chore(views): export create, update, remove componetn

--- src/app/views/notes/features/index.ts
+++ src/app/views/notes/features/index.ts
@@ -1,1 +1,4 @@
 export * from './notes-listing/notes-listing.component';
+export * from './update-note/update-note.component';
+export * from './create-note/create-note.component';
+export * from './remove-note/remove-note.component';
