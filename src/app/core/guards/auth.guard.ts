0af407be2c0f3804d2b73fb2bdcd99052236b7b0
wardvisual <wardvisual@gmail.com>
2023-08-15 20:33:48 +0800

    chore(guard): remove custom routing service and use built in

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,16 +1,14 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '@ng16-demoapp/services';
-import { RoutingService } from '@ng16-demoapp/services';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
-  const routingService = inject(RoutingService);
+  const router = inject(Router);
 
   if (authService.isAuthenticated()) {
     return true;
   }
 
-  routingService.redirectTo('signin');
-  return false;
+  return router.navigateByUrl('/signin');
 };
